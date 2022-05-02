42e28387594e7e070642f80d8323f98e20589c1e
Mark Stock <markjstock@gmail.com>
2022-05-02 17:39:16 -0400

    Added usage, command-line parsing, and test works

--- estOLS.cpp
+++ estOLS.cpp
@@ -13,71 +13,123 @@
 #include <chrono>
 #include <iomanip>	// for setprecision
 #include <iostream>
+#include <cctype>	// for isdigit
 
+//
+// basic usage
+//
+static void usage(char const *progname) {
+  std::cerr << "Usage:\n";
+  std::cerr << "  " << progname << " -x observations.csv -y response.csv [-o output.csv] [-t m n]\n\n";
+  std::cerr << "  where observations.csv is an n (rows) by m (columns) matrix where n > m,\n";
+  std::cerr << "  and response.csv is an n (rows) vector of responses/outcomes,\n";
+  std::cerr << "  alternatively use -t to perform a speed test with the given n and m.\n";
+  std::cerr << "  Output is in csv format and goes to stdout unless -o is used.\n";
+  exit(1);
+}
+
+//
+// entry and exit
+//
 int main(int argc, char const *argv[]) {
 
   std::cerr << "\nestOLS - Ordinary Least Squares Solver\n\n";
 
-  // read command line and get input
-  if (argc == 2) {
-    std::string infile = argv[1];
-  } else {
-    std::cerr << "\nUsage:\n";
-    std::cerr << "  " << argv[0] << " -x observations.csv -y response.csv\n\n";
-    std::cerr << "  where observations.csv is an n (rows) by m (columns) matrix where n > m" << std::endl;
-    std::cerr << "  and response.csv is an n (rows) vector of responses/outcomes" << std::endl;
-    return -1;
+  // default values
+  std::string inmatrixfile = "mat.csv";
+  std::string inobservfile = "observ.csv";
+  std::string outfile = "";
+  bool speedtest = false;
+  size_t n = 10;
+  size_t m = 2;
+
+  // parse the command-line arguments
+  for (int iarg=1; iarg<argc; ++iarg) {
+    if (strncmp(argv[iarg], "-x", 2) == 0) {
+      inmatrixfile = argv[++iarg];
+      std::cout << "input matrix file is " << inmatrixfile << "\n";
+    } else if (strncmp(argv[iarg], "-y", 2) == 0) {
+      inobservfile = argv[++iarg];
+      std::cout << "input observations file is " << inobservfile << "\n";
+    } else if (strncmp(argv[iarg], "-t", 2) == 0) {
+      if (std::isdigit(argv[iarg+1][0])) m = atoi(argv[++iarg]); else usage(argv[0]);
+      if (std::isdigit(argv[iarg+1][0])) n = atoi(argv[++iarg]); else usage(argv[0]);
+      // hold off on size checking until later
+      speedtest = true;
+      std::cout << "running speed test with m=" << m << " and n="<< n << "\n";
+    } else if (strncmp(argv[iarg], "-o", 2) == 0) {
+      outfile = argv[++iarg];
+      std::cout << "output weights file is " << inobservfile << "\n";
+    } else {
+      // fail on everything else
+      usage(argv[0]);
+    }
   }
+
+
 
   // write times to microsecond precision
   std::cerr << std::setprecision(6) << std::fixed;
   // and output with sci notation and many digits
   std::cout << std::setprecision(10);
 
-  // generate sample test data
-  const int32_t m = 100;
-  const int32_t n = 2000;
+  // set up our primary linear algebra variables
+  Eigen::MatrixXd Xmat;		// regression matrix
+  Eigen::VectorXd y;		// observations
+  Eigen::VectorXd B;		// Betas - the solution
 
-  // allocate and initialize
-  auto start = std::chrono::system_clock::now();
-  Eigen::MatrixXd A = Eigen::MatrixXd::Random(n, m);
-  if (n*m < 1000) std::cerr << "Here is the matrix A:\n" << A << std::endl;
-  Eigen::VectorXd b = Eigen::VectorXd::Random(n);
-  if (n*m < 1000) std::cerr << "Here is the right hand side b:\n" << b << std::endl;
-  auto end = std::chrono::system_clock::now();
-  std::chrono::duration<double> elapsed_seconds = end-start;
-  std::cerr << "Init time: \t[" << elapsed_seconds.count() << "] seconds" << std::endl;
+  if (speedtest) {
+
+    // allocate and initialize sample data
+    auto start = std::chrono::system_clock::now();
+    Xmat = Eigen::MatrixXd::Random(n, m);
+    if (n*m < 1000) std::cerr << "Here is the matrix Xmat:\n" << Xmat << std::endl;
+    y = Eigen::VectorXd::Random(n);
+    if (n*m < 1000) std::cerr << "Here is the right hand side y:\n" << y << std::endl;
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cerr << "Init time: \t[" << elapsed_seconds.count() << "] seconds" << std::endl;
+
+  } else {
+
+  }
 
   // calculate condition number
   // see https://stackoverflow.com/questions/33575478/how-can-you-find-the-condition-number-in-eigen
   if (false) {
-    start = std::chrono::system_clock::now();
-    Eigen::JacobiSVD<Eigen::MatrixXd> svd(A);
+    auto start = std::chrono::system_clock::now();
+    Eigen::JacobiSVD<Eigen::MatrixXd> svd(Xmat);
     double cond = svd.singularValues()(0) / svd.singularValues()(svd.singularValues().size()-1);
     std::cerr << "The condition number (from SVD) is: " << cond << std::endl;
-    end = std::chrono::system_clock::now();
-    elapsed_seconds = end-start;
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
     std::cerr << "CN time: \t[" << elapsed_seconds.count() << "] seconds" << std::endl;
   }
 
   // solve
 
   // this isn't working, but would be too slow anyway
-  //std::cout << "The least-squares solution is:\n" << A.template bdcSvd<Eigen::ComputeThinU | Eigen::ComputeThinV>().solve(b) << std::endl;
+  //std::cout << "The least-squares solution is:\n" << Xmat.template bdcSvd<Eigen::ComputeThinU | Eigen::ComputeThinV>().solve(y) << std::endl;
 
   // QR decomposition - compromise between speed and stability
-  start = std::chrono::system_clock::now();
-  std::cout << "The solution using the QR decomposition is:\n" << A.colPivHouseholderQr().solve(b) << std::endl;
-  end = std::chrono::system_clock::now();
-  elapsed_seconds = end-start;
-  std::cerr << "QR time: \t[" << elapsed_seconds.count() << "] seconds" << std::endl;
+  {
+    auto start = std::chrono::system_clock::now();
+    B = Xmat.colPivHouseholderQr().solve(y);
+    std::cout << "The solution using the QR decomposition is:\n" << B << std::endl;
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cerr << "QR time: \t[" << elapsed_seconds.count() << "] seconds" << std::endl;
+  }
 
-  // Normal equations - fastest but less stable/precise
-  start = std::chrono::system_clock::now();
-  std::cout << "The solution using normal equations is:\n" << (A.transpose() * A).ldlt().solve(A.transpose() * b) << std::endl;
-  end = std::chrono::system_clock::now();
-  elapsed_seconds = end-start;
-  std::cerr << "NEq time: \t[" << elapsed_seconds.count() << "] seconds" << std::endl;
+  {
+    // Normal equations - fastest but less stable/precise
+    auto start = std::chrono::system_clock::now();
+    B = (Xmat.transpose() * Xmat).ldlt().solve(Xmat.transpose() * y);
+    std::cout << "The solution using normal equations is:\n" << B << std::endl;
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end-start;
+    std::cerr << "NEq time: \t[" << elapsed_seconds.count() << "] seconds" << std::endl;
+  }
 
   // normal exit
   exit(0);
